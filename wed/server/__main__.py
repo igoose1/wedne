38c527140499c7b889801782ba9d1c1b8beaec0d
Oskar Sharipov <oskargit@riseup.net>
2023-06-28 03:48:55 +0800

    Add consume_tasks to CLI

--- wed/server/__main__.py
+++ wed/server/__main__.py
@@ -1,22 +1,49 @@
+import logging
+
 import fire
 import uvicorn
+from huey.consumer_options import ConsumerConfig
 
 from wed.server.settings import settings
+from wed.server.tasks import job_queue
 
 
-def main() -> None:
+class Main:
     """
-    Server entrypoint
+    Server for tower building
 
-    It's configured with environment variables or .env file."""
-    uvicorn.run(
-        "wed.server.api:app",
-        host=settings.host,
-        port=settings.port,
-        reload=settings.reload,
-    )
+    Everything is configured with environment variables or .env file.
+    """
+
+    def run_api(self) -> None:
+        """
+        Server entrypoint
+        """
+        uvicorn.run(
+            "wed.server.api:app",
+            host=settings.host,
+            port=settings.port,
+            reload=settings.reload,
+        )
+
+    def consume_tasks(self) -> None:
+        """
+        Run Huey Consumer
+
+        It's a task executor which is used for almost everything.
+        """
+        config = ConsumerConfig(
+            workers=1,
+            periodic=True,
+            max_delay=2.0,
+        )
+        logger = logging.getLogger("huey")
+        config.setup_logger(logger)
+
+        consumer = job_queue.create_consumer(**config.values)
+        consumer.run()
 
 
 if __name__ == "__main__":
     fire.core.Display = lambda lines, out: out.write("\n".join(lines) + "\n")  # type: ignore
-    fire.Fire(main, name="wed.server")
+    fire.Fire(Main(), name="wed.server")
