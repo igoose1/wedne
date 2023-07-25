581949f30c42dddbf5e79702b4825d11f473ca0b
Oskar Sharipov <oskargit@riseup.net>
2023-07-25 15:43:38 +0700

    Add telethon to requirements and use tdesktop keys

---
+++ wed/client/telegram_creds.py
@@ -0,0 +1,4 @@
+# This is official tdesktop snap's API key. It's obfuscated because I'm not
+# sure Telegram would be happy to see it here
+API_ID = 123456 + 487879
+API_HASH = "f".join(["c9ca14", "d1c48680", "73d4", "12d414b425d"])[::-1]
