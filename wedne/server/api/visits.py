84b6cabf4fb6f4520d8149c263300c79d7ca91a6
Oskar Sharipov <oskargit@riseup.net>
2023-08-08 21:41:13 +0700

    Fix "Take a Visit" when it could give no commands

--- wedne/server/api/visits.py
+++ wedne/server/api/visits.py
@@ -27,7 +27,7 @@ def take_a_visit(
         social_media_id=data.social_media_id,
     )
     raw_command = dao.get_command(
-        from_=now - timedelta(seconds=settings.seconds_of_delay),
+        from_=now - timedelta(seconds=settings.minutes_to_order),
         social_media_id=data.social_media_id,
     )
     if raw_command is None:
