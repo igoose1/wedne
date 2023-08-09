65cb6fc48c76fd32f3ecfbdfebc41cce619c7a90
Oskar Sharipov <oskargit@riseup.net>
2023-08-09 17:25:19 +0700

    Revert "Let FastAPI show a package version"
    This reverts commit 7635fc916a2227c9c3fa0a0e225a2ee7e7e3a010.

--- wedne/server/api/__init__.py
+++ wedne/server/api/__init__.py
@@ -1,12 +1,7 @@
-from importlib import metadata
-
 import fastapi
 
 from wedne.server.api import stats, visits
 
-app = fastapi.FastAPI(
-    title="wedne",
-    version=metadata.version("wedne"),
-)
+app = fastapi.FastAPI()
 app.include_router(stats.router, prefix="/stats")
 app.include_router(visits.router, prefix="/visits")
