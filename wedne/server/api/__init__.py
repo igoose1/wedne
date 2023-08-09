7635fc916a2227c9c3fa0a0e225a2ee7e7e3a010
Oskar Sharipov <oskargit@riseup.net>
2023-08-09 17:17:46 +0700

    Let FastAPI show a package version

--- wedne/server/api/__init__.py
+++ wedne/server/api/__init__.py
@@ -1,7 +1,12 @@
+from importlib import metadata
+
 import fastapi
 
 from wedne.server.api import stats, visits
 
-app = fastapi.FastAPI()
+app = fastapi.FastAPI(
+    title="wedne",
+    version=metadata.version("wedne"),
+)
 app.include_router(stats.router, prefix="/stats")
 app.include_router(visits.router, prefix="/visits")
